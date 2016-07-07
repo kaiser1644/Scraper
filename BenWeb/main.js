bc0f58b8ad965124f1d108fab0d4ca6829ff434f
Yao <kaiser1644@gmail.com>
2016-07-07 16:38:53 -0500

    Set up react.js and node.js framework

--- BenWeb/main.js
+++ BenWeb/main.js
@@ -3,11 +3,19 @@ var express = require("express");
 var fs = require("fs");
 var request = require("request");
 var cheerio = require("cheerio");
+var ReactDOMServer = require("react-dom/server");
+var React = require("react");
 
 // constants
 var BASE_URL = "http://forums.huaren.us";
 
 var app = express();
+
+var Home = React.createFactory(require("./client/public/index"));
+app.get("/", function (req, res) {
+	res.send(ReactDOMServer.renderToStaticMarkup(Home({})));
+});
+
 app.get("/scrape", function (req, res) {
 	
 	var url = "http://forums.huaren.us/showtopic.aspx?topicid=2036519";
