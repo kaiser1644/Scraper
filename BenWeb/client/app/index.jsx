b0cdc8927d89bece5690a320c666820ccfe0db6b
Yao <kaiser1644@gmail.com>
2016-07-08 01:02:42 -0500

    More react.js

--- BenWeb/client/app/index.jsx
+++ BenWeb/client/app/index.jsx
@@ -1,11 +1,53 @@
 var React = require("react");
+var Rb = require("react-bootstrap");
+
+// React-bootstrap components
+var PageHeader = Rb.PageHeader;
+var FormGroup = Rb.FormGroup;
+var FormControl = Rb.FormControl;
+var HelpBlock = Rb.HelpBlock;
+var ControlLabel = Rb.ControlLabel;
+var Button = Rb.Button;
+var Navbar = Rb.Navbar;
+var Nav = Rb.Nav;
+var MenuItem = Rb.MenuItem;
 
 module.exports = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Test
-      </div>
+	render: function() {
+		return (
+			<html>
+				<head>
+					<title>Ben</title>
+					<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
+				</head>
+				<body>
+					<div className="container">
+					
+					<Navbar>
+						<Navbar.Header>
+						  <Navbar.Brand>
+							Ben
+						  </Navbar.Brand>
+						  <Navbar.Toggle />
+						</Navbar.Header>
+						<Navbar.Collapse>
+						</Navbar.Collapse>
+					</Navbar>
+					
+					<form>
+						<FormGroup controlId="formControlsFile">
+							<ControlLabel>File</ControlLabel>
+							<FormControl type="file" />
+							<HelpBlock>Example block-level help text here.</HelpBlock>
+						</FormGroup>
+						<Button type="submit">
+							Start
+						</Button>
+					</form>
+					
+					</div>
+				</body>
+			</html>
     );
   }
 });
--- BenWeb/client/public/index.js
+++ BenWeb/client/public/index.js
@@ -1,15 +1,83 @@
 "use strict";
 
 var React = require("react");
+var Rb = require("react-bootstrap");
+
+// React-bootstrap components
+var PageHeader = Rb.PageHeader;
+var FormGroup = Rb.FormGroup;
+var FormControl = Rb.FormControl;
+var HelpBlock = Rb.HelpBlock;
+var ControlLabel = Rb.ControlLabel;
+var Button = Rb.Button;
+var Navbar = Rb.Navbar;
+var Nav = Rb.Nav;
+var MenuItem = Rb.MenuItem;
 
 module.exports = React.createClass({
-  displayName: "exports",
+	displayName: "exports",
 
-  render: function render() {
-    return React.createElement(
-      "div",
-      null,
-      "Test"
-    );
-  }
+	render: function render() {
+		return React.createElement(
+			"html",
+			null,
+			React.createElement(
+				"head",
+				null,
+				React.createElement(
+					"title",
+					null,
+					"Ben"
+				),
+				React.createElement("link", { rel: "stylesheet", href: "https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" })
+			),
+			React.createElement(
+				"body",
+				null,
+				React.createElement(
+					"div",
+					{ className: "container" },
+					React.createElement(
+						Navbar,
+						null,
+						React.createElement(
+							Navbar.Header,
+							null,
+							React.createElement(
+								Navbar.Brand,
+								null,
+								"Ben"
+							),
+							React.createElement(Navbar.Toggle, null)
+						),
+						React.createElement(Navbar.Collapse, null)
+					),
+					React.createElement(
+						"form",
+						null,
+						React.createElement(
+							FormGroup,
+							{ controlId: "formControlsFile" },
+							React.createElement(
+								ControlLabel,
+								null,
+								"File"
+							),
+							React.createElement(FormControl, { type: "file" }),
+							React.createElement(
+								HelpBlock,
+								null,
+								"Example block-level help text here."
+							)
+						),
+						React.createElement(
+							Button,
+							{ type: "submit" },
+							"Start"
+						)
+					)
+				)
+			)
+		);
+	}
 });
